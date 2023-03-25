f6ab061429e7e465bcbe12483d1e1d339f9f4a90
Эмина Какабаева <eminakakabaeva@ws-192-168-1-43.st-net.ru>
2023-03-25 22:28:43 +0300

    DZ4_3

---
+++ Program2.cs
@@ -0,0 +1,32 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+ 
+Console.WriteLine ("Для указания диапазона значений введите минимальное число");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Для указания диапазона значений введите максимальное число");
+int max = Convert.ToInt32(Console.ReadLine())+1;
+int [] numbers = new int[8];
+FillArray(numbers);
+ 
+void FillArray(int []collection)
+{
+    int index=0;
+    Console.Write("[");
+    while (index<8)
+    {
+        if (index<7)
+        {
+        collection[index]= new Random().Next(min,max);
+        Console.Write($"{collection[index]}, ");
+        index++;
+        }
+        if (index==7)
+        {
+        collection[index]= new Random().Next(min,max);
+        Console.Write($"{collection[index]}");
+        index++;
+        }
+    }
+    Console.Write("]");
+}
