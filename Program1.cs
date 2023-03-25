6fe108ab595a7e489f9886eb2a44dd54874869fc
Эмина Какабаева <eminakakabaeva@ws-192-168-1-43.st-net.ru>
2023-03-25 22:23:05 +0300

    DZ4_2

---
+++ Program1.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+ 
+ 
+Console.WriteLine("введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр введенного числа = {summa(num)}");
+ 
+double summa(int num)
+{
+    int sum = 0;
+    int result=sum;
+while (num > 0)
+    {
+        int div = num % 10;
+        num = num / 10;
+        sum = sum + div;
+        result=sum;
+       
+    }
+    return result;
+}
